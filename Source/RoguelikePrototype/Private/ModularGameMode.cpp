fa27913d9d5eb790919355fd5454ab1ba4a7f447
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2024-02-12 21:33:09 +0000

    Back to working on this project. Started moving to using C++ on this project and will be using it alongside blueprints, started working on a game mode base in C++ using Lyra as the example of how to use both

---
+++ Source/RoguelikePrototype/Private/ModularGameMode.cpp
@@ -0,0 +1,15 @@
+#include "ModularGameMode.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ModularGameMode)
+
+AModularGameModeBase::AModularGameModeBase(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	
+}
+
+AModularGameMode::AModularGameMode(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	
+}
